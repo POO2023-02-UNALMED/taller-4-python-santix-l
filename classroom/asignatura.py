6e926e464a373ffa51217590f57082f9f9f442af
santix-l <137665289+santix-l@users.noreply.github.com>
2023-09-02 15:25:18 -0500

    1

--- classroom/asignatura.py
+++ classroom/asignatura.py
@@ -1,8 +1,11 @@
 class Asignatura:
 
-    def __init__(self, nombre, salon):
+    def __init__(self, nombre, salon=None):
         self._nombre = nombre
         self._salon = salon
 
-    # def __str__(self):
-    #     pass
+    def __str__(self):
+        
+        if self._salon == None:
+
+            return self._nombre+" remoto"
